320680286629690bdbf802e89dabc49a2f39b51a
DmitryChesnokovv <gawr03@yandex.ru>
2024-07-17 10:49:48 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+course = "Python"
+total_tasks = 12
+hours_spent = 1.5
+averageExecutionTime = hours_spent / total_tasks
+print('Курс',course ,'всего задач',total_tasks ,'затрачено часов',hours_spent ,'часа','среднее время выполнения',averageExecutionTime ,'часа')
